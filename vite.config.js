305a3d154b6e464336864837c37e8c14e100cd52
Shubham <shubhamchakraborty702@gmail.com>
2026-07-06 19:34:01 +0530

    commit for the changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/flexoconapp/",
+  base: "/",
   server: {
-    port: 3000,
+    open: true,
   },
 });
